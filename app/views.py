560e383dbf450f4c8b07db03d267e370b190d57c
christinekarimi <karimikim3@gmail.com>
2018-04-03 11:55:22 +0300

    Control flow

--- app/views.py
+++ app/views.py
@@ -2,9 +2,13 @@ from flask import render_template
 from app import app
 
 #views
+@app.route('/')
 
-@app.route('/movie/<int:movie_id>')
-def movie(movie_id):
+def index():
 
-    
-    return render_template('movie.html',id = movie_id)
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+    title = 'Home - Welcome to The best Movie Review Website Online'
+    return render_template('index.html', title = title)
